523fc13da27ef3b2c1e22f4a27c8d6291fb5a0f8
quichesama <kmbehari@gmail.com>
2024-10-11 02:08:45 -0700

    add: begin stubbing out profile info

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,3 +1,33 @@
-<div>
-    profile ish
+<div class="flex-column">
+    <div class="flex-row">
+        <label>
+            First Name
+            <input type="text" placeholder="First Name" required/>
+        </label>
+        <label>
+            Last Name
+            <input type="text" placeholder="Last Name" required/>
+        </label>
+    </div>
+    <label>
+        Birth Date
+        <input type="date" placeholder="01/01/1980" required/>
+    </label>
 </div>
+
+<style>
+    input, label {
+        display:block;
+    }
+
+    .flex-column {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .flex-row {
+        display: flex;
+        flex-direction: row;
+    }
+    
+</style>
